09fc9b253dd1a4842eb4e828b319c61f17b19a9e
moonglazer <dltkdgus119@naver.com>
2019-05-25 16:45:34 +0900

    radix sort

---
+++ SortingEnv/radix_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void RadixSort(int num, int* array){
+
+}
+
+void sorting(int* array, int num, int e){
+    int output[num];
+    int count[10]={0};
+
+    for(int i=0;i<num;i++){
+        count[array[i]%(e*10)]++
+    }
+
+    for(int i=0;i<9;i++){
+        count[i+1]+=count[i];
+    }
+
+    for(int i=num-1;i>=0;i--){
+        output[count[array[num]%(10*e)]]=array[num];
+        count[array[num]%(10*e)]--;
+    }
+
+    for(int i=0;i<num; i++){
+        output[i]=array[i];
+    }
+}
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    int array*= (int*)malloc(sizeof(int)*num);
+    for(int i=0;i<num;i++){
+        scanf("%d", array[i]);
+    }
+}
