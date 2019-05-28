bc170bc308dae703be073f8c3b9390906568866e
moonglazer <dltkdgus119@naver.com>
2019-05-28 16:07:04 +0900

    modify 5/27

--- SortingEnv/radix_sort.c
+++ SortingEnv/radix_sort.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void RadixSort(int num, int* array){
-    int max=array[0];
-    for(int i=1;i>num;i++){
-        if(array[i]>max) max=array[i];
-    }
-    for(int e=1;e<max;e*=10){
-        Sorting(array, num, e);
-    }
-}
-
 void sorting(int* array, int num, int e){
     int output[num];
     int count[10]={0};
 
     for(int i=0;i<num;i++){
-        count[array[i]%(e*10)]++
+        count[(array[i]/e)%10]++;
     }
 
     for(int i=0;i<9;i++){
@@ -24,8 +14,8 @@ void sorting(int* array, int num, int e){
     }
 
     for(int i=num-1;i>=0;i--){
-        output[count[array[num]%(10*e)]]=array[num];
-        count[array[num]%(10*e)]--;
+        output[count[(array[i]/e)%10]-1]=array[num];
+        count[(array[i]/e)%10]--;
     }
 
     for(int i=0;i<num; i++){
@@ -33,12 +23,30 @@ void sorting(int* array, int num, int e){
     }
 }
 
+void RadixSort(int num, int* array){
+    int max=array[0];
+    for(int i=1;i>num;i++){
+        if(array[i]>max) max=array[i];
+    }
+    for(int e=1;e<max;e*=10){
+        sorting(array, num, e);
+    }
+}
+
 int main()
 {
     int num;
     scanf("%d", &num);
-    int array*= (int*)malloc(sizeof(int)*num);
+
+    int *array= (int*)malloc(sizeof(int)*num);
+
     for(int i=0;i<num;i++){
-        scanf("%d", array[i]);
+        scanf("%d", &array[i]);
+    }
+
+    RadixSort(num, array);
+
+    for(int i=0; i<num;i++){
+        printf("%d ", array[i]);
     }
 }
